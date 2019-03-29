b0803775f068668254d8bc6f68765236e30f1f48
luber <tomas.lubertino@gmail.com>
2019-03-29 14:26:59 -0300

    Se agrego la configuracion de firebase auth y ya se pueden registrar y logear usuarios

--- app/src/main/java/com/example/hypechat/LoginActivity.kt
+++ app/src/main/java/com/example/hypechat/LoginActivity.kt
@@ -2,15 +2,61 @@ package com.example.hypechat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private val TAG = "Login"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        auth = FirebaseAuth.getInstance()
+
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun validateField(field: TextInputLayout): Boolean {
+
+        val fieldStr = field.editText!!.text.toString().trim()
+
+        if (fieldStr.isEmpty()){
+            field.error = "The field can not be empty"
+            return false
+        } else {
+            field.error = null
+            return true
+        }
+    }
+
+    fun loginUser(view: View){
+
+        if (validateField(emailTextInputLayout) && validateField(passwordTextInputLayout)){
+
+            val email = emailTextInputLayout.editText!!.text.toString()
+            val password = passwordTextInputLayout.editText!!.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "signInWithEmail:success")
+                        Toast.makeText(this, "signInWithEmail:success!!!!", Toast.LENGTH_SHORT).show()
+                        val user = auth.currentUser
+                        //updateUI(user)
+                    } else {
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(this, "Authentication failed: ${task.exception}", Toast.LENGTH_SHORT).show()
+                        //updateUI(null)
+                    }
+                }
+        }
     }
 }
--- app/src/main/java/com/example/hypechat/RegistrationActivity.kt
+++ app/src/main/java/com/example/hypechat/RegistrationActivity.kt
@@ -2,15 +2,60 @@ package com.example.hypechat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private val TAG = "Registration"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
+        auth = FirebaseAuth.getInstance()
+
         setSupportActionBar(toolbarRegistration)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun validateField(field: TextInputLayout): Boolean {
+
+        val fieldStr = field.editText!!.text.toString().trim()
+
+        if (fieldStr.isEmpty()){
+            field.error = "The field can not be empty"
+            return false
+        } else {
+            field.error = null
+            return true
+        }
+    }
+
+    fun registerUser(view: View){
+
+        if (validateField(registerEmailTextInputLayout) && validateField(registerPasswordTextInputLayout)){
+            val email = registerEmailTextInputLayout.editText!!.text.toString()
+            val password = registerPasswordTextInputLayout.editText!!.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        //updateUI(user)
+                    } else {
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(this, "Authentication failed: ${task.exception}", Toast.LENGTH_SHORT).show()
+                        //updateUI(null)
+                    }
+                }
+        }
     }
 }
