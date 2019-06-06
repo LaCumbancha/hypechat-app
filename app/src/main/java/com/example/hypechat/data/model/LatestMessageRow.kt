aeca3fdd6b919d5ffb314a178c6775c878f47ff0
luber <tomas.lubertino@gmail.com>
2019-06-06 18:02:55 -0300

    Se agrego el formato para mostrar la fecha en los ultimos mensajes de chat. Se agrego el icono de chats sin leer

--- app/src/main/java/com/example/hypechat/data/model/LatestMessageRow.kt
+++ app/src/main/java/com/example/hypechat/data/model/LatestMessageRow.kt
@@ -1,11 +1,14 @@
 package com.example.hypechat.data.model
 
+import android.view.View
 import com.example.hypechat.R
 import com.example.hypechat.data.model.rest.response.ChatResponse
 import com.squareup.picasso.Picasso
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.latest_message_row.view.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class LatestMessageRow(val chat: ChatResponse): Item<ViewHolder>() {
 
@@ -16,6 +19,16 @@ class LatestMessageRow(val chat: ChatResponse): Item<ViewHolder>() {
     override fun bind(viewHolder: ViewHolder, position: Int) {
 
         viewHolder.itemView.latestMessageRowTextView.text = chat.content
+
+        val date = LocalDateTime.parse(chat.timestamp, DateTimeFormatter.RFC_1123_DATE_TIME)
+        val format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm")
+        val newDateFormat = date.format(format)
+        viewHolder.itemView.timeLatestMessageRowTextView.text = newDateFormat
+
+        if (chat.unseen){
+            viewHolder.itemView.notificationLatestMessageRowtextView.text = chat.offset.toString()
+            viewHolder.itemView.notificationLatestMessageRowtextView.visibility = View.VISIBLE
+        }
 
         viewHolder.itemView.fullnameLatestMessageRowTextView.text = chat.chatName
         Picasso.get().load(chat.chatPicture)
