c34f271e63ceb81fae850e808139f6664998d550
Tomas Lubertino <tomas.lubertino@gmail.com>
2019-06-22 10:55:54 -0300

    Se actualizaron los modelos de respuesta de messages, team y user

--- app/src/main/java/com/example/hypechat/data/model/rest/response/TeamResponse.kt
+++ app/src/main/java/com/example/hypechat/data/model/rest/response/TeamResponse.kt
@@ -9,6 +9,7 @@ data class TeamResponse (
     @SerializedName("location") val location : String?,
     @SerializedName("picture") val picture : String?,
     @SerializedName("id") val team_id : Int,
+    @SerializedName("messages") val messages : Int,
     @SerializedName("team_name") val team_name : String,
     @SerializedName("role") val role : String,
     @SerializedName("welcome_message") val welcome_message : String?
